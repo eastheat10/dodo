47194ea74670e34cabeda75902bd2057c6633923
윤동열 <eastheat10@gmail.com>
2022-06-10 00:14:29 +0900

    feature: UsersRepository 작성, 테스트

--- account-api/src/main/java/com/nhnacademy/accountapi/entity/Users.java
+++ account-api/src/main/java/com/nhnacademy/accountapi/entity/Users.java
@@ -7,10 +7,18 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class Users {
 
     @Id
@@ -24,4 +32,12 @@ public class Users {
 
     @Enumerated(EnumType.STRING)
     private Status status;
+
+    public void deleteUser() {
+        this.status = Status.WITHDRAWAL;
+    }
+
+    public void makeDormant() {
+        this.status = Status.DORMANT;
+    }
 }
---
+++ account-api/src/test/java/com/nhnacademy/accountapi/repository/UserRepositoryTest.java
@@ -0,0 +1,134 @@
+package com.nhnacademy.accountapi.repository;
+
+import static com.nhnacademy.accountapi.entity.Status.*;
+import static org.assertj.core.api.Assertions.*;
+
+import com.nhnacademy.accountapi.entity.Users;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Test
+    @DisplayName("사용자 등록")
+    void testUserRegister() {
+
+        Users users = Users.builder()
+                           .username("username")
+                           .password("password")
+                           .email("emaill@e.mail")
+                           .status(JOIN)
+                           .build();
+
+        Users save = usersRepository.save(users);
+
+        System.out.println(save.getStatus());
+
+        assertThat(save).isEqualTo(users);
+    }
+
+    @Test
+    @DisplayName("회원 조회")
+    void testReadUser() {
+
+        Users users = Users.builder()
+                           .username("username")
+                           .password("password")
+                           .email("emaill@e.mail")
+                           .status(JOIN)
+                           .build();
+
+        Users save = usersRepository.save(users);
+
+        Users findUser = usersRepository.findById(save.getId())
+                                        .orElseThrow(IllegalArgumentException::new);
+
+        assertThat(findUser).isEqualTo(save);
+    }
+
+    @Test
+    @DisplayName("회원 삭제")
+    void testDeleteUser() {
+
+        Users users = Users.builder()
+                           .username("username")
+                           .password("password")
+                           .email("emaill@e.mail")
+                           .status(JOIN)
+                           .build();
+
+        Users save = usersRepository.save(users);
+
+        save.deleteUser();
+        usersRepository.flush();
+
+        Users findUser = usersRepository.findById(save.getId())
+                                        .orElseThrow(IllegalArgumentException::new);
+
+        assertThat(findUser.getStatus()).isEqualTo(WITHDRAWAL);
+    }
+
+    @Test
+    @DisplayName("회원 휴면 처리")
+    void testUserStatusDormant() {
+
+        Users users = Users.builder()
+                           .username("username")
+                           .password("password")
+                           .email("emaill@e.mail")
+                           .status(JOIN)
+                           .build();
+
+        Users save = usersRepository.save(users);
+
+        save.makeDormant();
+        usersRepository.flush();
+
+        Users findUser = usersRepository.findById(save.getId()).get();
+
+        assertThat(findUser.getStatus()).isEqualTo(DORMANT);
+    }
+
+    @Test
+    @DisplayName("Email로 회원 찾기")
+    void testFindUserByEmail() {
+        Users users = Users.builder()
+                           .username("username")
+                           .password("password")
+                           .email("emaill@e.mail")
+                           .status(JOIN)
+                           .build();
+
+        Users save = usersRepository.save(users);
+
+        Users emailUser = usersRepository.findByEmail(save.getEmail())
+                                         .orElseThrow(IllegalArgumentException::new);
+
+        assertThat(emailUser).isEqualTo(save);
+    }
+
+    @Test
+    @DisplayName("로그인")
+    void testUserLogin() {
+
+        Users users = Users.builder()
+                           .username("username")
+                           .password("password")
+                           .email("emaill@e.mail")
+                           .status(JOIN)
+                           .build();
+
+        Users save = usersRepository.save(users);
+
+        Users loginUser = usersRepository.findByUsernameAndPassword(users.getUsername(), users.getPassword())
+                                         .orElseThrow(IllegalArgumentException::new);
+
+        assertThat(loginUser).isEqualTo(save);
+    }
+}
